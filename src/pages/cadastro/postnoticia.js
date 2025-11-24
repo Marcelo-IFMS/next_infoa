d4e1854aa020b9948975ba9a312f42d77ac8bea3
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2025-11-24 19:31:40 +0000

    modal

--- src/pages/cadastro/postnoticia.js
+++ src/pages/cadastro/postnoticia.js
@@ -2,13 +2,13 @@ import { useState } from 'react';
 import { Button, Form, Container, Row, Col } from 'react-bootstrap';
 
 export default function Postnoticia() {
-      const [titulonoticia, setTitulo] = useState("");
-      const [conteudonoticia, setConteudo] = useState("");
-      const [tiponoticia, setTipo] = useState("");
-    
-    const enviarPost = async (e) => {
-      e.preventDefault();
+    const [titulonoticia, setTitulo] = useState("");
+    const [conteudonoticia, setConteudo] = useState("");
+    const [tiponoticia, setTipo] = useState("");
 
+    const enviarPost = async (e) => {
+        e.preventDefault();
+        setLoading(true);
         const body = {
             titulonoticia,
             conteudonoticia,
@@ -29,6 +29,8 @@ export default function Postnoticia() {
         } catch (err) {
             console.error("Erro ao enviar:", err);
             alert("Erro ao enviar notícia.");
+        }finally {
+            setLoading(false);
         }
     }
     return (
@@ -39,17 +41,17 @@ export default function Postnoticia() {
                     <Form onSubmit={enviarPost}>
                         <Form.Group className="mb-3" controlId="titulonoticia">
                             <Form.Label>Título da Notícia</Form.Label>
-                            <Form.Control type="text" className='bg-warning-subtle' placeholder="Informe o Título da Notícia"  onChange={(e) => setTitulo(e.target.value)} />
+                            <Form.Control type="text" className='bg-warning-subtle' placeholder="Informe o Título da Notícia" onChange={(e) => setTitulo(e.target.value)} />
                             <Form.Text className="text-muted">
                                 Informe de forma clara o título da notícia.
                             </Form.Text>
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="conteudonoticia">
                             <Form.Label>Informe a Notícia</Form.Label>
-                            <Form.Control as="textarea" rows={3} className='bg-warning-subtle' onChange={(e) => setConteudo(e.target.value)}/>
+                            <Form.Control as="textarea" rows={3} className='bg-warning-subtle' onChange={(e) => setConteudo(e.target.value)} />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="tiponoticia">
-                            <Form.Select aria-label="Selecione o tipo de notícia" className='bg-warning-subtle'  onChange={(e) => setTipo(e.target.value)}>
+                            <Form.Select aria-label="Selecione o tipo de notícia" className='bg-warning-subtle' onChange={(e) => setTipo(e.target.value)}>
                                 <option>Selecione o Tipo da Notícia</option>
                                 <option value="Ciência">Ciência</option>
                                 <option value="Educação">Educação</option>
