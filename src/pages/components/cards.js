b158887bcda9f9c071e70f0f38b3994c2b6a33cc
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2025-11-27 18:26:50 +0000

    p

--- src/pages/components/cards.js
+++ src/pages/components/cards.js
@@ -8,19 +8,7 @@ export default function Cards({noticia}) {
     return <>
         <Col key={noticia.idnoticia}>
             <Card>
-                <Card.Header className="text-center fw-bold bg-warning-subtle">
-                    <a href={`/noticias/`}>{/* altera aqui */}{noticia.titulonoticia}</a>
-                </Card.Header>
-                <Card.Body>
-                    <Card.Title className="text-capitalize">
-                        <a href={`/noticias/tipo/${noticia.tiponoticia}`}>{noticia.tiponoticia}</a>
-                    </Card.Title>
-                    <Card.Text className="text-truncate">
-                        {noticia.conteudonoticia}
-                    </Card.Text>
-                </Card.Body>
-                <Card.Footer>{new Date(noticia.datahoracadastro).toLocaleString("pt-br")}</Card.Footer>
-            </Card>
+              </Card>
         </Col>
     </>
 }
