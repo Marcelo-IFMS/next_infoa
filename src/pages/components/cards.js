96e78630a25ff511fdb889bdfd8b8253fa96e470
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2025-11-27 18:23:47 +0000

    {}

--- src/pages/components/cards.js
+++ src/pages/components/cards.js
@@ -1,8 +1,26 @@
-
+import { Card, Col } from "react-bootstrap";
+import { usePathname } from "next/navigation";
 
 export default function Cards({noticia}) {
- console.log(noticia);
+    const pathname = usePathname();
+
+    const isHome = pathname === "/";
     return <>
-       
+        <Col key={noticia.idnoticia}>
+            <Card>
+                <Card.Header className="text-center fw-bold bg-warning-subtle">
+                    <a href={`/noticias/${noticia.idnoticia}`}>{/* altera aqui */}{noticia.titulonoticia}</a>
+                </Card.Header>
+                <Card.Body>
+                    <Card.Title className="text-capitalize">
+                        <a href={`/noticias/tipo/${noticia.tiponoticia}`}>{noticia.tiponoticia}</a>
+                    </Card.Title>
+                    <Card.Text className="text-truncate">
+                        {noticia.conteudonoticia}
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>{new Date(noticia.datahoracadastro).toLocaleString("pt-br")}</Card.Footer>
+            </Card>
+        </Col>
     </>
 }
