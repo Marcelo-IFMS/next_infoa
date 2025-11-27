db51318c388394f0ed201daeb152e922e289d25c
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2025-11-27 14:25:10 +0000

    alert off

--- src/pages/cadastro/postnoticia.js
+++ src/pages/cadastro/postnoticia.js
@@ -27,7 +27,7 @@ export default function Postnoticia() {
 
             const result = await response.json();
             console.log("API respondeu:", result);
-            //alert("Notícia enviada com sucesso!");
+            
         } catch (err) {
             console.error("Erro ao enviar:", err);
             //alert("Erro ao enviar notícia.");
