92b06224bb3b097d99d2f33371c69aa24774fff8
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2025-11-27 18:16:16 +0000

    teste

--- src/pages/components/cards.js
+++ src/pages/components/cards.js
@@ -6,21 +6,6 @@ export default function Cards({noticia}) {
 
     const isHome = pathname === "/";
     return <>
-        <Col key={noticia.idnoticia}>
-            <Card>
-                <Card.Header className="text-center fw-bold bg-warning-subtle">
-                    <a href={`/noticias/${noticia.idnoticia}`}>{/* altera aqui */}{noticia.titulonoticia}</a>
-                </Card.Header>
-                <Card.Body>
-                    <Card.Title className="text-capitalize">
-                        <a href={`/noticias/tipo/${noticia.tiponoticia}`}>{noticia.tiponoticia}</a>
-                    </Card.Title>
-                    <Card.Text className="text-truncate">
-                        {noticia.conteudonoticia}
-                    </Card.Text>
-                </Card.Body>
-                <Card.Footer>{new Date(noticia.datahoracadastro).toLocaleString("pt-br")}</Card.Footer>
-            </Card>
-        </Col>
+        ${noticia.idnoticia}
     </>
 }
