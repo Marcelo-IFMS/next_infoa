f06b7cf5317cddb957de11b1f3e47f41dba351b3
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2025-11-27 18:19:29 +0000

    a

--- src/pages/components/cards.js
+++ src/pages/components/cards.js
@@ -1,10 +1,7 @@
-import { Card, Col } from "react-bootstrap";
-import { usePathname } from "next/navigation";
+
 
 export default function Cards({noticia}) {
-    const pathname = usePathname();
-
-    const isHome = pathname === "/";
+ 
     return <>
         {noticia.idnoticia}
     </>
