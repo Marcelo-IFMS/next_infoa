5bf92679ad48c8381e3b7e4bc1d9d85d32851359
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2025-11-27 20:06:17 +0000

    back

--- src/pages/components/cards.js
+++ src/pages/components/cards.js
@@ -1,14 +1,34 @@
+"use client";
 import { Card, Col } from "react-bootstrap";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from 'react';
 
-export default function Cards({noticia}) {
+export default function Cards(noticia) {
+    const [truncate, setTruncate] = useState("");
     const pathname = usePathname();
-
-    const isHome = pathname === "/";
+    useEffect(() => {
+        if (pathname === "/") {
+            setTruncate("text-truncate");
+        } else {
+            setTruncate("");
+        }
+    }, [pathname]);
     return <>
         <Col key={noticia.idnoticia}>
             <Card>
-              </Card>
+                <Card.Header className="text-center fw-bold bg-warning-subtle">
+                    <a href={`/noticias/${noticia.idnoticia}`}>{/* altera aqui */}{noticia.titulonoticia}</a>
+                </Card.Header>
+                <Card.Body>
+                    <Card.Title className="text-capitalize">
+                        <a href={`/noticias/tipo/${noticia.tiponoticia}`}>{noticia.tiponoticia}</a>
+                    </Card.Title>
+                    <Card.Text className={truncate}>
+                        {noticia.conteudonoticia}
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>{new Date(noticia.datahoracadastro).toLocaleString("pt-br")}</Card.Footer>
+            </Card>
         </Col>
     </>
 }
