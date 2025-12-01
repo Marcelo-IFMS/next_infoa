699837351aa30e91a95f939fd7375b18afca8cba
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2025-12-01 13:42:26 +0000

    tabele delete

--- src/pages/components/table.js
+++ src/pages/components/table.js
@@ -1,11 +1,15 @@
 "use client";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from 'react';
+import { Modal } from "react-bootstrap";
 
 export default function Tablelist(props) {
     const [truncate, setTruncate] = useState("");
     const pathname = usePathname();
+    const [ResultadoCadastro, setResultadoCadastro] = useState("");
+    const [show, setShow] = useState(false);
 
+    const handleClose = () => setShow(false);
     useEffect(() => {
         if (pathname === "/") {
             setTruncate("text-truncate");
@@ -32,17 +36,38 @@ export default function Tablelist(props) {
                 });
                 const result = await response.json();
                 if (!result.status)
-                    alert("Erro ao deletar notícia:");
-                alert("Notícia deletada com sucesso!");
+                    setResultadoCadastro("Erro ao deletar notícia");
+                setResultadoCadastro("Notícia apagada com sucesso!");
                 // Atualizar a página após a exclusão
-               // window.location.reload();
+                window.location.reload();
+                setShow(true);
             } catch (err) {
-                alert("Erro ao deletar notícia:" + err);
+                setResultadoCadastro("Erro ao deletar notícia: " + err);
+                setShow(true);
             }
         }
     }
     return (
         <>
+            {/* Modal */}
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal title</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {ResultadoCadastro}
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Fechar
+                    </Button>
+                </Modal.Footer>
+            </Modal>
             <td className="text-capitalize">
                 <a href={`/noticias/${props.idnoticia}`}>
                     {props.idnoticia}
@@ -57,9 +82,9 @@ export default function Tablelist(props) {
                 {props.conteudonoticia}
             </td>
             <td>
-                  {props.datahoracadastro && !isNaN(new Date(props.datahoracadastro))
-                        ? new Date(props.datahoracadastro).toISOString().replace("T", " ").substring(0, 19)
-                        : "Data inválida"}
+                {props.datahoracadastro && !isNaN(new Date(props.datahoracadastro))
+                    ? new Date(props.datahoracadastro).toISOString().replace("T", " ").substring(0, 19)
+                    : "Data inválida"}
             </td>
             <td>
                 update
