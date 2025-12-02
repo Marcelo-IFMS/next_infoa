14cd34be1b296b44c327951dfaae71afa3b94950
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2025-12-02 19:55:55 +0000

    testes

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,9 @@ import Cards from './components/cards';
 export async function getServerSideProps() {
   // Fetch data from external API
   const res = await fetch('https://api6anoticias.vercel.app/noticias')
-  // const repo = await res.json()
+  const repo = await res.json()
   // Pass data to the page via props
-  // console.log(repo);
-  return { props: { noticias: res } }
+  return { props: { noticias: repo } }
 }
 export default function Home({ noticias }) {
   return (<>
