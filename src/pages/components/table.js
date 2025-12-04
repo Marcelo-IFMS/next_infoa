0ea687cc9ed4258fa50d92287c2b2aec66ebaf5b
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2025-12-04 21:10:34 +0000

    date

--- src/pages/components/table.js
+++ src/pages/components/table.js
@@ -136,7 +136,7 @@ export default function Tablelist(props) {
                     {props.conteudonoticia}
                 </td>
                 <td>
-                    {new Date(props.datahoracadastro).toLocaleDateString()}
+                    {(props.datahoracadastro).toLocaleDateString()}
                 </td>
                 <td>
                     <a href="#" onClick={openUpdateModal}>update</a>
